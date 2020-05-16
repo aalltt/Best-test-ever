0611a23d4ad87fec819dd3e620ed0c4246f5ed13
aalltt <geekjjssww@list.ru>
2020-05-16 11:01:55 +0300

    add all f

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println(2);
         System.out.println(3);
         System.out.println("update1");
+        System.out.println("111");
+        System.out.println(2222);
 
 
     }
